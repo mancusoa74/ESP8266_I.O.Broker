3c51a848c39d01650f30412d80f3c0684066ba78
Lstt2005 <admin-23@bk.ru>
2015-05-06 08:43:26 +0300

    Update and rename dimmer.php to dimmer_method.php

--- scripts/dimmer_method.php
+++ scripts/dimmer_method.php
@@ -1,6 +1,6 @@
 <?php
 $ip = gg("ESP8266009b63a3.wanip");
-$channel = 0;
+$channel = 0; //для других каналов, соответственно, 1 или 2 
 $url = "http://$ip/pwm?ch=0&set=" . gg("EESSPP.pwm" . $channel);
 getURL($url, 0);
 $pwm=gg("EESSPP.pwm".$channel);
