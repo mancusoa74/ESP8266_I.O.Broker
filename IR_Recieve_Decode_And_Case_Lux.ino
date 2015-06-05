1d5ec0460e114e26c534b26285756ed441afda75
Lstt2005 <admin-23@bk.ru>
2015-06-05 19:20:56 +0300

    Update and rename IR_Recieve_Decode_And_Case.ino to IR_Recieve_Decode_And_Case_Lux.ino

--- IR_Recieve_Decode_And_Case_Lux.ino
+++ IR_Recieve_Decode_And_Case_Lux.ino
@@ -1,9 +1,12 @@
-//урок от Дмитрия Осипова - управление arduino ИК пультом
 #include <IRremote.h> // это скачанная библиотека
+#include <Wire.h>
+#include <BH1750.h>
+BH1750 lightMeter;
 
 int RECV_PIN = 11; //вход ИК приемника
 IRrecv irrecv(RECV_PIN);
 decode_results results;
+int luxold=0;
 int a=0; // переменная переключения режима, при нажатии кнопки она принимает значение +1
 int b=0; //
 int c=0; //
@@ -25,15 +28,36 @@ pinMode(5, OUTPUT);
 pinMode(6, OUTPUT);
 pinMode(7, OUTPUT);
 
+//////////////////////////
+Serial.begin(9600);
+ lightMeter.begin();
+ Serial.println("Running...");
+////////////////////////////////////////
 } 
-
 void loop() {
-if (irrecv.decode(&results)) {
+/////////////////////////////////////////////////////////////
+uint16_t lux = lightMeter.readLightLevel();
+ if (lux>35) {  //проверяем, если значение датчика света больше 35, только тогда, если яркость изменилась, выодим её текущее знач.
+ if (lux!=luxold) {Serial.print("Light: "); Serial.print(lux); Serial.println(" lx");}
+ }
+  //Serial.print(lux);
+ // Serial.println(" lx");
+ 
+  //delay(10);
+  
+  ////////////////////////////////////////////////////////////
+ if (irrecv.decode(&results)) {
 delay(3); // задержка перед выполнением определения кнопок, чтобы избежать быстрое двойное нажатие
-
-if (results.value == 0x55173B8B) {a=a+1;} // обработка нажитя клавиши, здесь переменная принимает значение +1
+  Serial.print("Code ");
+  Serial.print(results.value, HEX);
+  Serial.println (" ");
+  Serial.print("Light: ");
+  Serial.print(lux);
+  Serial.println(" lx");
+  //if (results.value == 0x55173B8B) {a=a+1;} // обработка нажитя клавиши, здесь переменная принимает значение +1
 if (results.value == 0xF6A075A7) {a=a+1;} // обработка нажитя клавиши, здесь переменная принимает значение +1
-if (results.value == 0x1008889) {b=b+1;} //ВАЖНО !!! ( 1008889 ) это код кнопки №2 - моего пульта от телевизора Panasonic - у вашего пульта будет другой код - замените на свой
+//if (results.value == 0xCD9DF683) {b=b+1;} //ВАЖНО !!! ( 1008889 ) это код кнопки №2 - моего пульта от телевизора Panasonic - у вашего пульта будет другой код - замените на свой
+if (results.value == 0x2C14BC67) {b=b+1;} //ВАЖНО !!! ( 1008889 ) это код кнопки №2 - моего пульта от телевизора Panasonic - у вашего пульта будет другой код - замените на свой
 if (results.value == 0x1004849) {c=c+1;}
 if (results.value == 0x100C8C9) {d=d+1;}
 if (results.value == 0x1002829) {e=e+1;}
@@ -43,8 +67,9 @@ if (results.value == 0x1006869) {g=g+1;}
 // начинаем включать светодиоды на пинах
 if (a==1){digitalWrite(2, HIGH);} else {digitalWrite(2, LOW); a=0;} // действие после нажатия кнопки, если переменная стала равна 1 то
 if (a==1){digitalWrite(13, HIGH);} else {digitalWrite(13, LOW); a=0;} // действие после нажатия кнопки, если переменная стала равна 1 то
-//if (b==1){digitalWrite(2, HIGH);} else {digitalWrite(2, LOW); b=0;} // произвести переключение пина в режим HIGH, если же нет то вернуть переменную в исходное состояние
-if (c==1){digitalWrite(3, HIGH);} else {digitalWrite(3, LOW); c=0;} // Дествие может быть другим
+if (b==1){digitalWrite(3, HIGH);} else {digitalWrite(3, LOW); b=0;} // произвести переключение пина в режим HIGH, если же нет то вернуть переменную в исходное состояние
+//if (b==1){digitalWrite(13, HIGH);} else {digitalWrite(13, LOW); b=0;} // произвести переключение пина в режим HIGH, если же нет то вернуть переменную в исходное состояние
+//if (c==1){digitalWrite(3, HIGH);} else {digitalWrite(3, LOW); c=0;} // Дествие может быть другим
 if (d==1){digitalWrite(4, HIGH);} else {digitalWrite(4, LOW); d=0;} // действие после нажатия кнопки
 if (e==1){digitalWrite(5, HIGH);} else {digitalWrite(5, LOW); e=0;} // действие после нажатия кнопки
 if (f==1){digitalWrite(6, HIGH);} else {digitalWrite(6, LOW); f=0;} // действие после нажатия кнопки
@@ -57,5 +82,7 @@ delay(10); //пауза между повторами //
 irrecv.resume(); // 
 
 }
-
+luxold=lux;
 }
+
+ 
