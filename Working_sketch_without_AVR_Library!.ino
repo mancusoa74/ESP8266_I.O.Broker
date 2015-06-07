71e79799f0427c89c9b35e3283f9772701101ed2
Lstt2005 <admin-23@bk.ru>
2015-06-07 10:29:33 +0300

    Update Working_sketch_without_AVR_Library!.ino

--- Working_sketch_without_AVR_Library!.ino
+++ Working_sketch_without_AVR_Library!.ino
@@ -38,4 +38,109 @@
     attachInterrupt(0,IRinterrupt,FALLING); // назначим прерывание 
  } 
 
-  
+    void loop() { 
+      if(IR_KEY) { 
+      //Serial.println(IR_KEY,HEX); //выводим код кнопки в терминал при отладке . 
+        IR_KEY = 0; 
+      } 
+    } 
+     
+ /* обработчик прерывания*/ 
+ void IRinterrupt(){ 
+    static unsigned long key, prevTime; 
+    int I=0; 
+    unsigned long currTime, d; 
+    currTime = micros(); 
+    d = currTime - prevTime; 
+    if(d < 1100) return;        // "дребезг" 
+    if(d < 1400){              // "0" 
+      key = key << 1; 
+      prevTime = currTime; 
+      return; } 
+    if(d < 2400){              // "1" 
+      key = key << 1; 
+      key++; 
+      prevTime = currTime; 
+      return;} 
+    if(d < 13000) IR_KEY = key; // код кнопки получен 
+    prevTime = currTime; // ПОЛУЧЕНИЕ КОДА С ПУЛЬТА 
+     
+    switch (IR_KEY) 
+ { 
+ case NEXT: 
+ mp3.next();// СЛЕДУЩИЙ ТРЭК 
+   {   
+     break; 
+   } 
+ case PAUSE: 
+ mp3.pause(); // пауза 
+   { 
+    break; 
+   } 
+ case STOP: 
+ mp3.stop(); // стоп 
+   { 
+    break; 
+   } 
+ case PLAY_SD: 
+ mp3.play_sd(0x0001); // ПЕРЕКЛЮЧАЕМ НА КАРТУ 
+   { 
+    break; 
+   } 
+ case PLAY_USB_DISK: 
+ mp3.play_usb_disk(0x0001); // ПЕРЕКЛЮЧАЕМ НА ФЛЕШКУ 
+   { 
+    break; 
+   }   
+ case VOLUME_10: 
+   mp3.volume(0x16);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_20: 
+   mp3.volume(0x17);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_30: 
+   mp3.volume(0x18);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_40: 
+   mp3.volume(0x19);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_50: 
+   mp3.volume(0x1A);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_60: 
+   mp3.volume(0x1B);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_70: 
+   mp3.volume(0x1C);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_80: 
+   mp3.volume(0x1D);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_90: 
+   mp3.volume(0x1E);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ case VOLUME_100: 
+   mp3.volume(0x1F);// УРОВЕНЬ ГРОМКОСТИ 
+   {   
+     break; 
+   } 
+ } 
+ } 
