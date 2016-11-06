8e8ce10d967379904de9703d66e77e0b59fe4b44
A M <anmancus@cisco.com>
2016-11-06 14:47:43 +0100

    properly create library for arduino ide

--- ESP8266_Standalone/main/TembooAccount.h
+++
@@ -1,3 +0,0 @@
-#define TEMBOO_ACCOUNT "xxxx"  // your Temboo account name 
-#define TEMBOO_APP_KEY_NAME "myFirstApp"  // your Temboo app key name
-#define TEMBOO_APP_KEY  "133a2334a5884b62b0e2c8b99106fff4"  // your Temboo app key
